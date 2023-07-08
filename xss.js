f2b22a654a027eb12c83d1830b625fe4d7585301
Junyor Serra <110888027+serrajunyor@users.noreply.github.com>
2023-07-07 21:28:17 -0300

    Add files via upload

---
+++ xss.js
@@ -0,0 +1,13 @@
+// Username
+let username = 'Junyor 🚀';
+// User simpledescription etc (48 characters allowed)
+let description = 'pls don't waste my time.';
+// User avatar
+let avatar = 'https://xatimg.com/image/PeskBEDV0lhz.png';
+
+
+
+
+document.getElementById('username').innerHTML = username;
+document.getElementById('description').innerHTML = description;
+document.getElementById('userAvatar').innerHTML = `<img src="${avatar}" class="xp_profile_img">`;
